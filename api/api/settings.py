1ebff1de821a0ae4f89c18a899368fb70f7dc7bc
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-09-22 15:10:50 +0300

    update settings.py - Added render to allowed hosts

--- api/api/settings.py
+++ api/api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-6eu)g25u-l#*e@&g#z-$c!35km++1m98j1vn&46yofk*or)23+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["savannah-informatics-back-end-challenge.onrender.com"]
 
 
 # Application definition
